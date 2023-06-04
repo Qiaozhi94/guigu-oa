7fd75a27e2aede2858efb92d08b5de20ebc48d41
GeorgeL <qiaozhi_li@126.com>
2023-06-05 01:34:08 +0800

    更新代码

--- service/src/main/java/com/qiaozhi/auth/controller/SysRoleController.java
+++ service/src/main/java/com/qiaozhi/auth/controller/SysRoleController.java
@@ -4,6 +4,9 @@ package com.qiaozhi.auth.controller;
 import com.qiaozhi.auth.service.SysRoleService;
 import com.qiaozhi.common.result.Result;
 import com.qiaozhi.model.system.SysRole;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,9 @@ import java.util.List;
 /**
  * @author Georg
  */
+
+
+@Api("角色管理接口")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -24,6 +30,7 @@ public class SysRoleController {
 
 
     //统一返回数据结果
+    @ApiOperation("查询所有的角色")
     @GetMapping("/findALL")
     public Result findAll(){
         List<SysRole> list = sysRoleService.list();
