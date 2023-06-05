0fcd0401b6c9a1c899b42af03fb473e0774489ca
GeorgeL <qiaozhi_li@126.com>
2023-06-06 00:42:28 +0800

    更新角色分页查询接口

--- service/src/main/java/com/qiaozhi/auth/ServiceAuthApplication.java
+++ service/src/main/java/com/qiaozhi/auth/ServiceAuthApplication.java
@@ -4,9 +4,13 @@ package com.qiaozhi.auth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author Georg
+ */
 @SpringBootApplication
-@MapperScan("com.qiaozhi.auth.mapper")
+@ComponentScan("com.qiaozhi")
 public class ServiceAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceAuthApplication.class, args);
